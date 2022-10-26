06f1c732719cec7bb75f9e2a9af7ad20d4a4fc2f
FedericoRubbi <federico.rubbi@yandex.com>
2022-10-26 09:31:17 +0200

    Build directory removed and .gitignore edit.

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,5 +1,11 @@
 # Copy main compiled file to rp2040-zero
-cp "$PWD/build/src/main.uf2" "/media/$USER/RPI-RP2/main.uf2"
-# Open serial with delay
-sleep 10
+echo "Loading program..."
+if cp "$PWD/build/src/main.uf2" "/media/$USER/RPI-RP2/main.uf2" ; then
+    echo "Program loaded."
+else
+    echo "Error in loading."
+fi
+# Open serial
+echo "Opening serial..."
+sleep 3
 minicom -b 9600 -o -D /dev/ttyACM0
